02b5684a26baa17db79acc66521a7f9ce369a814
Stalin Macias <mags961224hjccmt00@gmail.com>
2022-06-25 18:50:02 -0500

    Allocating initial token balance to traders' accounts

--- migrations/2_deploy_dex_contracts.js
+++ migrations/2_deploy_dex_contracts.js
@@ -24,6 +24,47 @@ module.exports = async function(deployer) {
         dexContract.addToken(hemiSymbol,hemiContract.address),
         dexContract.addToken(tetraSymbol,tetraContract.address)
     ])
+
+    // Allocating initial tokens to the trader's accounts
+
+    const accounts = await web3.eth.getAccounts();
+    const [admin,trader1,trader2,trader3,trader4] = [accounts[0],accounts[1],accounts[2],accounts[3],accounts[4]]
+
+    const amount = web3.utils.toWei('1000');
+
+    const seedTokenBalance = async (token,trader) => {
+        await token.faucet(trader,amount,{from:trader});
+        await token.approve(dexContract.address,amount,{from:trader});
+        const tokenSymbol = await token.symbol();
+        await dexContract.deposit(web3.utils.fromAscii(tokenSymbol),amount,{from:trader})
+        
+        //const tokenBalance = await dexContract.tokenBalances(trader1,web3.utils.fromAscii(tokenSymbol))
+        //console.log("Token Balancess: ", web3.utils.fromWei(tokenBalance));
+    }
+
+    // Seeding trader1
+    await Promise.all(
+        [daiContract, hemiContract, tetraContract].map(token => seedTokenBalance(token,trader1))
+    );
+
+    // Seeding trader2
+    await Promise.all(
+        [daiContract, hemiContract, tetraContract].map(token => seedTokenBalance(token,trader2))
+    );
+
+    // Seeding trader3
+    await Promise.all(
+        [daiContract, hemiContract, tetraContract].map(token => seedTokenBalance(token,trader3))
+    );
+
+    // Seeding trader4
+    await Promise.all(
+        [daiContract, hemiContract, tetraContract].map(token => seedTokenBalance(token,trader4))
+    );
+
+    
+    //
+
     
   };
   
