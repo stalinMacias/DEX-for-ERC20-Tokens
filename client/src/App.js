38ad0388e3701fd7239d9cb33d31549a49099787
Stalin Macias <mags961224hjccmt00@gmail.com>
2022-06-26 21:35:21 -0500

    Connected the frontend to the blockchain - created a function to create contract's instances dinamically to all the allowed ERC20 Tokens in the DEX Contract

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { getWeb3, getAccount , getNetworkId, getDexContractInstance, getTokensContractInstances } from './utils';
 
 function App() {
+  const text = async () => {
+    const web3 = await getWeb3();
+    console.log(await getAccount())
+    console.log(await getDexContractInstance(web3))
+    console.log(await getTokensContractInstances(web3))
+    return(
+      <p> hello </p>
+    )
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {text()}
     </div>
+
   );
 }
 
